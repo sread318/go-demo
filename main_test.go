9f2790ed6baf05cd0e0234ebae1ebe84262f200a
Tyler Conlee <tylerlconlee@gmail.com>
2017-07-13 14:54:39 -0700

    added response code test

--- main_test.go
+++ main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
@@ -31,6 +33,19 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
+func TestEmptyTable(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/posts", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	if body := response.Body.String(); body != "[]" {
+		t.Errorf("Expected an empty array. Got %s", body)
+	}
+}
+
 func ensureTableExists() {
 	if _, err := a.DB.Exec(tableCreationQuery); err != nil {
 		log.Fatal(err)
@@ -40,6 +55,19 @@ func ensureTableExists() {
 func clearTable() {
 	a.DB.Exec("DELETE FROM posts")
 	a.DB.Exec("ALTER SEQUENCE posts_id_seq RESTART WITH 1")
+}
+
+func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+	rr := httptest.NewRecorder()
+	a.Router.ServeHTTP(rr, req)
+
+	return rr
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
 }
 
 const tableCreationQuery = `CREATE TABLE IF NOT EXISTS posts

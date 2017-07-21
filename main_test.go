b5030654fd3b5e4b22a662d4df7c7afc18cf0145
Tyler Conlee <tylerlconlee@gmail.com>
2017-07-21 14:47:29 -0700

    Finished test writing

--- main_test.go
+++ main_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 )
 
@@ -43,6 +46,119 @@ func TestEmptyTable(t *testing.T) {
 
 	if body := response.Body.String(); body != "[]" {
 		t.Errorf("Expected an empty array. Got %s", body)
+	}
+
+}
+
+func TestGetNonExistentPost(t *testing.T) {
+	clearTable()
+
+	req, _ := http.NewRequest("GET", "/post/13", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+
+	var m map[string]string
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["error"] != "Post not found" {
+		t.Errorf("Expected the 'error' key of the response to be set to 'Post not found'")
+	}
+
+}
+
+func TestCreatePost(t *testing.T) {
+	clearTable()
+
+	payload := []byte(`{"name":"test post","author":"test author","body":"test content"}`)
+
+	req, _ := http.NewRequest("POST", "/post", bytes.NewBuffer(payload))
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusCreated, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+	if m["name"] != "test post" {
+		t.Errorf("Expected post title to be 'test post'. Got %v", m["name"])
+	}
+	if m["author"] != "test author" {
+		t.Errorf("Expected post author to be 'test author'. Got %v", m["author"])
+	}
+	if m["body"] != "test content" {
+		t.Errorf("Expected post content to be 'test content'. Got %v", m["body"])
+	}
+	if m["id"] != 1.0 {
+		t.Errorf("Expected post ID to be '1'. Got %v", m["id"])
+	}
+}
+
+func TestGetPost(t *testing.T) {
+	clearTable()
+	addPost(1)
+
+	req, _ := http.NewRequest("GET", "/post/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func TestUpdatePost(t *testing.T) {
+	clearTable()
+	addPost(1)
+
+	req, _ := http.NewRequest("GET", "/post/1", nil)
+	response := executeRequest(req)
+	var originalPost map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &originalPost)
+
+	payload := []byte(`{"name":"test post - updated name","author":"test author - updated name","body":"test content - updated content"}`)
+
+	req, _ = http.NewRequest("PUT", "/post/1", bytes.NewBuffer(payload))
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	var m map[string]interface{}
+	json.Unmarshal(response.Body.Bytes(), &m)
+
+	if m["name"] == originalPost["name"] {
+		t.Errorf("Expected post title to be %v. Got %v", originalPost["name"], m["name"])
+	}
+	if m["author"] == originalPost["author"] {
+		t.Errorf("Expected author to be %v. Got %v", originalPost["author"], m["author"])
+	}
+	if m["body"] == originalPost["body"] {
+		t.Errorf("Expected body to be %v. Got %v", originalPost["body"], m["body"])
+	}
+	if m["id"] != originalPost["id"] {
+		t.Errorf("Expected the id to remain the same (%v). Got %v", originalPost["id"], m["id"])
+	}
+}
+
+func TestDeletePost(t *testing.T) {
+	clearTable()
+	addPost(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("DELETE", "/product/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/product/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+}
+
+func addPost(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO posts(name, author, body) VALUES($1, $2, $3)", "Post "+strconv.Itoa(i), "Author "+strconv.Itoa(i), "Test content for post "+strconv.Itoa(i))
 	}
 }
 
